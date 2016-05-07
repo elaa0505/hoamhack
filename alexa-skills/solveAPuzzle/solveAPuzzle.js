c447e16eac786d24709a19765d732a8b1942faed
Geoffrey Wright <Geoff@MacBook-Pro-5.fritz.box>
2016-05-07 21:41:53 +0200

    answering questions kind of

--- alexa-skills/solveAPuzzle/solveAPuzzle.js
+++ alexa-skills/solveAPuzzle/solveAPuzzle.js
@@ -2,6 +2,26 @@
  * This function prompts a user to solve a puzzle to turn off their alarm
  */
 
+// global var with puzzles for the sleepy person to solve
+var puzzles = [
+    {
+        question: "what the fifth digit of pi is.",
+        answer: "9"
+    },
+    {
+        question: "what's two cubed?",
+        answer: "8"
+    },
+    {
+        question: "how many electoral votes does california have?",
+        answer: "55"
+    },
+    {
+        question: "what's the fourth planet from the sun?",
+        answer: "mars"
+    }
+]
+
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
 // etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = function (event, context) {
@@ -92,7 +112,7 @@ function getWelcomeResponse(callback) {
     // If we wanted to initialize the session to have some attributes we could add those here.
     var sessionAttributes = {};
     var cardTitle = "Welcome";
-    var speechOutput = "Good morning human, I hope you had a nice sleepy time. " +
+    var speechOutput = "Good morning, I hope you had a nice sleepy time. " +
         "You can turn off the alarm by saying off";
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
@@ -105,7 +125,7 @@ function getWelcomeResponse(callback) {
 
 function handleSessionEndRequest(callback) {
     var cardTitle = "Session Ended";
-    var speechOutput = "You have sucessfully turned off the alarm. Have a nice day!";
+    var speechOutput = "You got it! Air high five! Have a nice day!";
     // Setting this to true ends the session and exits the skill.
     var shouldEndSession = true;
 
@@ -113,22 +133,16 @@ function handleSessionEndRequest(callback) {
 }
 
 function silenceAlarm(intent, session, callback) {
-    var repromptText = 'I didn\'t get that...' ;
-    var sessionAttributes = {};
+    var sessionAttributes = {
+        questionCount: 0
+    };
+    var currentQuestion = puzzles[sessionAttributes.questionCount].question
+    var formatAnswerLike = 'You can can answer the question by saying, it is foo or the answer is bar' 
+    var repromptText = 'I didn\'t get that. I wanted to know ' + 
+        currentQuestion + formatAnswerLike;
     var shouldEndSession = false;
-    var speechOutput = "";
-
-    if (session.attributes) {
-        favoriteColor = session.attributes.favoriteColor;
-    }
-
-    if (favoriteColor) {
-        speechOutput = "Your favorite color is " + favoriteColor + ". Goodbye.";
-        shouldEndSession = true;
-    } else {
-        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
-            " is red";
-    }
+    var speechOutput = "Here's the deal human. I'll turn off the alarm as soon as you tell me " 
+         + currentQuestion;
 
     // Setting repromptText to null signifies that we do not want to reprompt the user.
     // If the user does not respond or says something that is not understood, the session
@@ -138,21 +152,28 @@ function silenceAlarm(intent, session, callback) {
 }
 
 function answerPuzzle(intent, session, callback) {
-    var repromptText = 'I didn\'t get that...' ;
-    var sessionAttributes = {};
+    var repromptText = ""; //'I didn\'t get that...' ;
+    var sessionAttributes = {
+        questionCount: session.questionCount
+    };
     var shouldEndSession = false;
+    var currentAnswer = puzzles[sessionAttributes.questionCount].answer;
+    var userAnswer = intent.slots.Answer.value;
     var speechOutput = "";
 
-    if (session.attributes) {
-        favoriteColor = session.attributes.favoriteColor;
-    }
-
-    if (favoriteColor) {
-        speechOutput = "Your favorite color is " + favoriteColor + ". Goodbye.";
-        shouldEndSession = true;
+    // Check if the users answer is correct
+    // use soft equals to coerce types, yay JavaScript
+    if (currentAnswer == userAnswer) {
+        // end session which will tell the user they are correct
+        // handleSessionEndRequest()
+        speechOutput = "Trello world"
     } else {
-        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
-            " is red";
+        //tell the user the correct answer and move onto the next question
+        sessionAttributes.questionCount += 1;
+        var nextQuestion = puzzles[sessionAttributes.questionCount].question;
+        speechOutput = "Oh no, the answer was " + currentAnswer + 
+            ". Moving on; " + nextQuestion;
+
     }
 
     // Setting repromptText to null signifies that we do not want to reprompt the user.
--- alexa-skills/solveAPuzzle/updateLambda.sh
+++ alexa-skills/solveAPuzzle/updateLambda.sh
@@ -1,6 +1,6 @@
 FUNNAME=solveAPuzzle;
 FILENAME=$FUNNAME.js
-s3BUCKET=hoamhack-alexa-skills;
+#s3BUCKET=hoamhack-alexa-skills;
 zip $FUNNAME $FILENAME
 #aws s3 cp $FUNNAME.zip s3://$s3BUCKET
 aws lambda update-function-code --function-name $FUNNAME --zip-file fileb://$FUNNAME.zip
